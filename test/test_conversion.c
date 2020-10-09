b9ef26fef5b92a654d89f36890593aa63b1f8e57
stepin105190 <69082292+stepin105190@users.noreply.github.com>
2020-10-09 11:55:49 +0530

    Update test_conversion.c

--- test/test_conversion.c
+++ test/test_conversion.c
@@ -10,7 +10,7 @@ long int Bin_to_Oct(long int);
 long int Bin_to_Hex(long int);
 long int Dec_to_Bin(long int);
 long int Dec_to_Oct(long int);
-long int Dec_to_Hex(long int);
+long int Dec_to_Hex(long int);      
 long int Oct_to_Bin(long int);
 long int Oct_to_Dec(long int);
 long int Oct_to_Hex(long int);
